262e1e3651cecd5a814ddf1427aadb80c67ccbf9
Lasse Benn Nørregaard <lassenorregaard@gmail.com>
2019-09-20 10:51:35 +0200

    Refactor helper row definitions

--- user/lassebn.h
+++ user/lassebn.h
@@ -5,7 +5,7 @@
 
 #define _______ KC_TRNS
 
-#define ___________________BLANK___________________ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+#define ______________BLANK_______________ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 
 /*
  * ,----------------------------------.                     ,----------------------------------.
@@ -31,8 +31,8 @@
  * | PgDn | Left | Down | Right|      |
  * `------+------+------+------+------´
  */
-#define _______________NAVIGATION_R1_______________ KC_PGUP, LGUI(KC_LEFT), KC_UP,   LGUI(KC_RIGHT), KC_TRNS
-#define _______________NAVIGATION_R2_______________ KC_PGDN, KC_LEFT,       KC_DOWN, KC_RGHT,        KC_TRNS
+#define ___________NAVIGATION_R1__________ KC_PGUP, LGUI(KC_LEFT), KC_UP,   LGUI(KC_RIGHT), KC_TRNS
+#define ___________NAVIGATION_R2__________ KC_PGDN, KC_LEFT,       KC_DOWN, KC_RGHT,        KC_TRNS
 
 /*
  * ,----------------------------------.                     ,----------------------------------.
@@ -43,13 +43,13 @@
  * | SHFT |   ^  |   [  |   ]  |   `  |                     |   \  |   1  |   2  |   3  |   0  |
  * `------+------+------+------+------´                     `------+------+------+------+------´
  */
-#define _____________NUMS_AND_SYMS_L1______________ KC_TRNS,   KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR
-#define _____________NUMS_AND_SYMS_L2______________ KC_TRNS,   KC_DLR,  KC_LPRN, KC_RPRN, KC_TILD
-#define _____________NUMS_AND_SYMS_L3______________ KC_LSHIFT, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV
+#define _________NUMS_AND_SYMS_L1_________ KC_TRNS,   KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR
+#define _________NUMS_AND_SYMS_L2_________ KC_TRNS,   KC_DLR,  KC_LPRN, KC_RPRN, KC_TILD
+#define _________NUMS_AND_SYMS_L3_________ KC_LSHIFT, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV
 
-#define _____________NUMS_AND_SYMS_R1______________ KC_SLSH, KC_7,    KC_8,   KC_9,   KC_TRNS
-#define _____________NUMS_AND_SYMS_R2______________ KC_PIPE, KC_4,    KC_5,   KC_6,   GUI_T(KC_DOT)
-#define _____________NUMS_AND_SYMS_R3______________ KC_BSLS, KC_1,    KC_2,   KC_3,   KC_0
+#define _________NUMS_AND_SYMS_R1_________ KC_SLSH, KC_7,    KC_8,   KC_9,   KC_TRNS
+#define _________NUMS_AND_SYMS_R2_________ KC_PIPE, KC_4,    KC_5,   KC_6,   GUI_T(KC_DOT)
+#define _________NUMS_AND_SYMS_R3_________ KC_BSLS, KC_1,    KC_2,   KC_3,   KC_0
 
 /*
  * ,----------------------------------.                     ,----------------------------------.
@@ -60,9 +60,9 @@
  * |      |  F1  |  F2  |  F3  |  F4  |                     |      |      |      |      |      |
  * `------+------+------+------+------´                     `------+------+------+------+------´
 */
-#define ________________FUNCTION_L1________________ KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12
-#define ________________FUNCTION_L2________________ KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8
-#define ________________FUNCTION_L3________________ KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4
+#define ____________FUNCTION_L1___________ KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12
+#define ____________FUNCTION_L2___________ KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8
+#define ____________FUNCTION_L3___________ KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4
 
-#define ________________FUNCTION_R1________________ KC_TRNS, KC_VOLD, KC_TRNS, KC_VOLU, KC_TRNS
-#define ________________FUNCTION_R2________________ KC_TRNS, KC_MRWD, KC_MPLY, KC_MFFD, KC_TRNS
+#define ____________FUNCTION_R1___________ KC_TRNS, KC_VOLD, KC_TRNS, KC_VOLU, KC_TRNS
+#define ____________FUNCTION_R2___________ KC_TRNS, KC_MRWD, KC_MPLY, KC_MFFD, KC_TRNS
