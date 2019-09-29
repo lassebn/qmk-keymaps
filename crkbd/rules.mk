a413363b588d76018550d73a1e41721df062bb3b
Lasse Benn Nørregaard <lassenorregaard@gmail.com>
2019-09-29 20:30:59 +0200

    Update rules

--- crkbd/rules.mk
+++ crkbd/rules.mk
@@ -29,3 +29,8 @@ SRC +=  ./lib/glcdfont.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
+
+MCU = atmega32u4
+F_CPU = 16000000
+ARCH = AVR8
+BOOTLOADER = caterina
