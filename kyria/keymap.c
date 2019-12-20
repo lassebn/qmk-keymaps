fcbbd54be22da0748cef024d0f2baf431c3680b4
Lasse Benn Nørregaard <lassenorregaard@gmail.com>
2019-12-20 18:11:12 +0100

    Remove layout comment

--- kyria/keymap.c
+++ kyria/keymap.c
@@ -29,20 +29,6 @@ enum layers {
 #define SPCFUN    LT(_FUNCTION, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* 
- * Base Layer: QWERTY
- *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |RAIS/ESC|   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/BS |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |  - _   |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | GUI  | Del  | Enter| Space| Esc  |  | Enter| Space| Tab  | Bksp | AltGr|
- *                        |      |      | Alt  | Lower| Raise|  | Lower| Raise|      |      |      |
- *                        `----------------------------------'  `----------------------------------'
- */
     [_QWERTY] = LAYOUT(
       _______, CTL_T(KC_Q), KC_W,    KC_E,     KC_R,    KC_T,                                     KC_Y,            KC_U,    KC_I,    KC_O,    CTL_T(KC_P),    _______,
       _______, GUI_T(KC_A), KC_S,    KC_D,     KC_F,    KC_G,                                     KC_H,            KC_J,    KC_K,    KC_L,    GUI_T(KC_SCLN), _______,
